e8ceaf67429273c58a847789681fb92bef8e7561
Ruben Jing <jing.ruben@gmail.com>
2025-04-17 22:26:04 -0700

    make dispBoard public so it can be accessed in main

--- include/board.h
+++ include/board.h
@@ -13,15 +13,17 @@ class Board {
 private:
     Square board[3][3];
     int filled;
-    void dispBoard();
 
 public:
     Board ();
+
+    void dispBoard();
+    
     bool isEmpty (int x, int y); 
     bool isGameOver () const;
     bool isWinningMove (int x, int y, Square player) const;
     void move (int x, int y, Square player);
-    
+
     std::vector<std::pair<int, int>> getAvailableMoves () const; 
     std::string getStateString () const;
     Square getSquare(int x, int y) const;
